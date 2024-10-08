32389a300da9dcb461d4aae137940fb3de69bf5b
Adnan Haider <Adussrk@gmail.com>
2024-10-08 14:41:29 +0530

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234343
+let accountEmail = "adusrk@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+accountEmail = "adussrk@gmail.com"
+accountPassword = "54321"
+accountCity = "Bangalore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+// number, bigint, string, boolean, null, undefined, etc
+
+let state
+let a = "12345"
+let b = 12345
+let c = null
+
+console.log(typeof state, typeof a, typeof b, typeof c)
+//undefined, string, number, object
